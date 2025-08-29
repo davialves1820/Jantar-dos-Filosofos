2480645b9301fff8b54add44aae14912201fda8c
Davi Alves Rodrigues <144604229+davialves1820@users.noreply.github.com>
2025-08-29 21:57:04 +0000

    problema do jantar dos filosofos

--- src/main.c
+++ src/main.c
@@ -1,7 +1,25 @@
-#include <stdio.h>
+#include "solucao_deadlock.h"
 
 int main(void) {
 
-    printf("Hello world");
+    pthread_t threads[TAM];
+    int ids[TAM];
+
+    // Criacao das threads (Filosofos)
+    for (int i = 0; i < TAM; i++) {
+        ids[i] = i;
+        pthread_create(&threads[i], NULL, vida_filosofo_deadlock, &ids[i]);
+    }
+
+    // Avisa no terminal quando ocorrer o deadlock
+    sleep(10);
+    printf("Se voce esta vendo esta mensagem e nada mais aconteceu, provavelmente houve um deadlock.\n");
+
+    // Espera as threads terminarem(nunca ocorre nesse caso)
+    for (int i = 0; i < TAM; i++) {
+        pthread_join(threads[i], NULL);
+    }
+
+    
     return 0;
 }
---
+++ src/solucao_deadlock.c
@@ -0,0 +1,46 @@
+#include "solucao_deadlock.h"
+
+pthread_mutex_t garfos[TAM]; // 
+
+// Pega o indice do garfo da esquerda
+int esquerda(int i) {
+    return i;
+}
+
+// Pega o indice do garfo da direita
+int direita(int i) {
+    return (i+1) % TAM;
+}
+
+// Funcao principal de cada thread(filosofo)
+void *vida_filosofo_deadlock(void *arg) {
+    int id = *(int*)arg; // ID do filosofo
+
+    while (1) {
+        printf("Filosofo %d esta pensando...\n", id);
+
+        pthread_mutex_lock(&garfos[esquerda(id)]);
+        printf("Filosofo %d pegou o garfo da esquerda\n", id);
+
+        pthread_mutex_lock(&garfos[direita(id)]);
+        printf("Filosofo %d pegou o gardo da direita e esta comendo.\n", id);
+
+        sleep(2); // Simula tempo para comer
+
+        // Devolve os garfos, primeiro o da direita, depois o da esquerda
+        pthread_mutex_unlock(&garfos[direita(id)]);
+        pthread_mutex_unlock(&garfos[esquerda(id)]);
+        
+        printf("Filosofo %d terminou de comer e devolveu os garfos.\n", id);
+    }
+
+    return NULL;
+}
+
+// Inicializa todos os mutexes
+__attribute__((constructor)) // Para chamar essa funcao antes da main
+void init_deadlock(void) {
+    for (int i = 0; i < TAM; i++) {
+        pthread_mutex_init(&garfos[i], NULL);
+    }
+}
