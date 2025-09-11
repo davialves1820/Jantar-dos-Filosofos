b1bde6f76b0e5299a0549a70074eaf501180a19b
davi20 <davialvesr18@gmail.com>
2025-09-11 13:12:30 +0000

    Adicao do looping na interface, para melhor experiencia.

--- src/main.c
+++ src/main.c
@@ -12,34 +12,43 @@ int main(void) {
 
     int escolha;
     
-    // Interface simples no terminal
-    printf("1- DEADLOCK\n");
-    printf("2- MONITOR\n");
-    printf("3- SEMAFORO\n");
-    printf("4- SEMAFORO COM CONTROLE\n");
-    printf("Opcao: ");
-    scanf("%d", &escolha);
+    do {
+        
+        // Interface simples no terminal
+        printf("1- DEADLOCK\n");
+        printf("2- MONITOR\n");
+        printf("3- SEMAFORO\n");
+        printf("4- SEMAFORO COM CONTROLE\n");
+        printf("0- SAIR\n");
+        printf("Opcao: ");
+        scanf("%d", &escolha);
 
-    switch (escolha) {
-        case 1:
-            init_deadlock();
-            break;
+        switch (escolha) {
+            case 1:
+                init_deadlock();
+                break;
 
-        case 2:
-            init_monitor();
-            break;
+            case 2:
+                init_monitor();
+                break;
 
-        case 3:
-            init_semaforo();
-            break;
+            case 3:
+                init_semaforo();
+                break;
 
-        case 4:
-            init_semaforo_controle();
-            break;
+            case 4:
+                init_semaforo_controle();
+                break;
 
-        default:
-            printf("Opcao invalida\n");
-    }
+            case 0:
+                printf("Tchau :)\n");
+                break;
+            
+            default:
+                printf("Opcao invalida\n");
+        }
+    } while (escolha != 0);
+    
 
     return 0;
 }
