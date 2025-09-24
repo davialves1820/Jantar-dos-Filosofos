00f381c556a7e70b99285d5c994eef3f77c6bd4b
ilyrsa <donatollarissa@gmail.com>
2025-09-24 09:19:15 -0300

    ajeitando o menu.

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include "solucao_semaforo.h"
 #include "solucao_monitor.h"
 #include "solucao_semaforo_controle.h"
+#include <stdio.h>
 
 // Definições globais para métricas (definidas aqui para evitar múltiplas definições)
 int refeicoes[TAM] = {0};
@@ -13,39 +14,60 @@ int main(void) {
     int escolha;
     
     do {
-        
-        // Interface simples no terminal
-        printf("1- DEADLOCK\n");
-        printf("2- MONITOR\n");
-        printf("3- SEMAFORO\n");
-        printf("4- SEMAFORO COM CONTROLE\n");
-        printf("0- SAIR\n");
-        printf("Opcao: ");
+        printf("\n");
+        printf("   .-.  .--.  .-. .-. .---.  .--.  .----.    .----.  .----.  .----.   .----..-..-.    .----.  .----. .----. .----..----.  .----.\n");
+        printf(".-.| | / {} \\ |  `| |{_   _}/ {} \\ | {}  }   | {}  \\/  {}  \\{ {__     | {_  | || |   /  {}  \\{ {__  /  {}  \\| {_ /  {}  \\{ {__  \n");
+        printf("| {} |/  /\\  \\| |\\  |  | | /  /\\  \\| .-. \\   |     /\\      /.-._} }   | |   | || `--.\\      /.-._} }\\      /| |  \\      /.-._} }\n");
+        printf("`----'`-'  `-'`-' `-'  `-' `-'  `-'`-' `-'   `----'  `----' `----'    `-'   `-'`----' `----' `----'  `----' `-'   `----' `----'\n");
+        printf("\n");
+        printf("========================================\n");
+        printf("  1 - Deadlock\n");
+        printf("  2 - Monitor\n");
+        printf("  3 - Semáforo\n");
+        printf("  4 - Semáforo com Controle\n");
+        printf("  0 - Sair\n");
+        printf("========================================\n");
+        printf("Escolha uma opção: ");
         scanf("%d", &escolha);
 
         switch (escolha) {
             case 1:
                 init_deadlock();
+                printf("\nPressione ENTER para continuar...");
+                while (getchar() != '\n'); // Limpa buffer
+                getchar();
                 break;
 
             case 2:
                 init_monitor();
+                printf("\nPressione ENTER para continuar...");
+                while (getchar() != '\n');
+                getchar();
                 break;
 
             case 3:
                 init_semaforo();
+                printf("\nPressione ENTER para continuar...");
+                while (getchar() != '\n');
+                getchar();
                 break;
 
             case 4:
                 init_semaforo_controle();
+                printf("\nPressione ENTER para continuar...");
+                while (getchar() != '\n');
+                getchar();
                 break;
 
             case 0:
-                printf("Tchau :)\n");
+                printf("\nTchau :)\n");
                 break;
             
             default:
-                printf("Opcao invalida\n");
+                printf("Opção inválida! Tente novamente.\n");
+                printf("\nPressione ENTER para continuar...");
+                while (getchar() != '\n');
+                getchar();
         }
     } while (escolha != 0);
     
